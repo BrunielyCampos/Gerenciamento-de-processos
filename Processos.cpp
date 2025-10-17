a874d5ad275855f3cb5405c3bbd6463877636353
BrunielyCampos <brunielycampos@gmail.com>
2025-10-17 16:27:28 -0300

    Criação dos métodos de Processos.cpp

---
+++ Processos.cpp
@@ -0,0 +1,32 @@
+#include "Processos.h"
+    
+    int Processos::getIdProcessso () const{
+        
+    }
+    int Processos::getPAlta () const{
+
+    }
+    int Processos::getPMedia () const{
+
+    }
+    int Processos::getPBaixa () const{
+
+    }
+    int Processos::getData () const{
+
+    }
+    int Processos::getHora () const{
+
+    }
+    string Processos::getAssunto () const{
+
+    } //confirmar se precisa de todos os gets
+
+    void Processos::abrirProcesso(){
+
+    } //cria processos
+
+
+    void imprimir() const{
+
+    }
