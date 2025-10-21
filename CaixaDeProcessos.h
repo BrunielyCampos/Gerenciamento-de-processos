c0391e6bca66d0284a6f7e4628a23cbf28b93e77
jardelna <jardel.a@escolar.ifrn.edu.br>
2025-10-21 08:25:14 -0300

    Criação de novas classes de Listas
    Listas duplamente encadeadas de Baixa e Média prioridade

---
+++ CaixaDeProcessos.h
@@ -0,0 +1,17 @@
+#ifndef _CAIXA_PROCESSOS_
+#define _CAIXA_PROCESSOS_
+
+
+class Pilha {
+    public:
+
+    private:
+    Pilha();
+
+    void empilhar();
+    void desempilhar();
+    void vazia();
+    void exibir();
+};
+
+#endif
