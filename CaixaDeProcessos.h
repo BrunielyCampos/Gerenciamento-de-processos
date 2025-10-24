72ce37f28d8cd9823a98ba2a955131594a28b38e
jardelna <jardel.a@escolar.ifrn.edu.br>
2025-10-24 15:26:51 -0300

    Update CaixaDeProcessos.h

--- CaixaDeProcessos.h
+++ CaixaDeProcessos.h
@@ -1,19 +1,3 @@
-#ifndef _PILHA_H_
-#define _PILHA_H_
-
-class Pilha {
-    public:
-
-    private:
-    Pilha();
-
-    void empilhar();
-    void desempilhar();
-    void vazia();
-    void exibir();
-};
-
-#endif
 #ifndef _CAIXA_PROCESSOS_
 #define _CAIXA_PROCESSOS_
 
