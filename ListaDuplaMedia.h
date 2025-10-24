e710d477c120c420e6f9e28bfbd894da11bbfbe8
BrunielyCampos <brunielycampos@gmail.com>
2025-10-24 12:52:35 -0300

    União das branchs

---
+++ ListaDuplaMedia.h
@@ -0,0 +1,28 @@
+#ifndef _LISTADUPLAMEDIA_H_
+#define _LISTADUPLAMEDIA_H_
+
+//confirmar se vai herdar alguma classe
+
+#include <iostream>
+
+
+struct Node{
+    Node * next;
+    Node * previous;
+};
+
+class ListaDuplaMedia {
+    private:
+    Node * head;
+    Node * tail;
+
+    public:
+    ListaDuplaMedia();
+
+    ~ListaDuplaMedia();
+
+    bool isEmpty();
+    void clear();
+};
+
+#endif
